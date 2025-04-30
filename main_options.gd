2c00d6819c56b4a9622e2b21580ab99b9fc31f66
pamy1q <britneyojos@gmail.com>
2025-04-30 19:52:54 +0100

    Deleted broken options_menu scene and cleaned references

--- main_options.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-
-# Called when the node enters the scene tree for the first time.
-@onready var options_menu_scene = preload("res://options_menu.tscn")
-func _on_options_button_pressed():
-	var options_menu = options_menu_scene.instantiate()
-	add_child(options_menu)# opens the options when function called .
